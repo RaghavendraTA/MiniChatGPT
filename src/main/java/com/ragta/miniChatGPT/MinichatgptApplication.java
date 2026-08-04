f475e9e927b7fb24ded12e412ae9d94b6e914214
temkarstudios <temkarstudios@gmail.com>
2026-08-04 22:54:35 +0530

    First commit

--- src/main/java/com/ragta/miniChatGPT/MinichatgptApplication.java
+++ src/main/java/com/ragta/miniChatGPT/MinichatgptApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MychatgptApplication {
+public class MinichatgptApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MychatgptApplication.class, args);
+		SpringApplication.run(MinichatgptApplication.class, args);
 	}
 
 }
